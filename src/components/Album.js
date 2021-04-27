4270992048a838cbf998110981cd3a1b09b75c24
Kurban <bdobik@mail.ru>
2021-04-27 15:41:05 +0300

    add css-class active for album-item

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectId} from "../redux/actionss";
 
 function Album(props) {
     const dispatch = useDispatch();
+    const selectedId = useSelector(state => state.albums.selectedId);
+
 
     function handleSelectedId() {
         dispatch(selectId(props.album.id))
     }
 
     return (
-        <li className='album' onClick={handleSelectedId}>
+        <li className={selectedId === props.album.id ? 'album active' : 'album'} onClick={handleSelectedId}>
             {props.album.title}
         </li>
     );
